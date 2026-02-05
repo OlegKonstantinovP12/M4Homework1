7fe69ffd7951ed5aa755ba1a8ef6db3b81170757
Oleg Konstantinov <oleg.boss@gmail.com>
2026-02-05 15:04:48 +0100

    Initial Commit

---
+++ M4Homework1/M4Homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  M4Homework1App.swift
+//  M4Homework1
+//
+//  Created by Oleg Konstantinov on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct M4Homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
